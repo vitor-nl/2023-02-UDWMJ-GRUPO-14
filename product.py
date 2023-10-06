320ff39d69bcee79867428cba712acfafb7401ed
Nicolas <nicolasfgiovanella539@gmail.com>
2023-10-06 20:22:02 -0300

    primeiro commit

---
+++ product.py
@@ -0,0 +1,9 @@
+class Product():
+    def __init__(self, name, descripit, date_fabrication, is_active, category):
+        self.name = name
+        self.descripit = descripit
+        self.date_fabrication = date_fabrication
+        self.is_active = is_active
+        self.is_active = is_active
+
+

4d93777d734d624d5887853f7b0f4fdbdc598ffc
Nicolas Giovanella <Nicolas@sopal.local>
2023-10-09 14:52:32 -0300

    add classe categoria

---
+++ category.py
@@ -0,0 +1,8 @@
+class Category():
+    def __init__(self, name, descripit, date_fabrication, is_active, category):
+        self.name = name
+        self.descripit = descripit
+        self.date_fabrication = date_fabrication
+        self.is_active = is_active
+        self.is_active = is_active
+
